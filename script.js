52bc43e0b9702378fb41a2582cd1ef8892da0524
Kikkentt <kentanthonydulangon@gmail.com>
2024-09-02 20:49:09 +0800

    Added 3 greeting messages at the end

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function showAlert(){
     var name = document.getElementById("userName").value;
     alert("Hello, " + name + "!");
 }
+
+console.log("Shimenet");
+console.log("Shimenet");
+console.log("Shimenet");
